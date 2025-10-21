918261346c6b0e529c422506a5da3f3a38d988f3
wangyang <1749748955@qq.com>
2025-10-21 15:41:42 +0800

    update

--- script/4a0f2cb3-3b1d-4a73-b965-892a0be7ca1b/main.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-script_dir = os.path.dirname(os.path.abspath(__file__))
-common_dir = os.path.abspath(os.path.join(script_dir, "../../script/common"))
-sys.path.insert(0, common_dir)
-
--- script/b2cf5de7-a94d-4b44-84b7-7f0dd4c1fcef/abundance_difference.py
+++
@@ -1,5 +0,0 @@
-
-
-
-def parse_data(request_param):
-    pass
