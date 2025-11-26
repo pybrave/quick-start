f8c1e7f3199cc3e2a750941666b9b7947e9005c9
wangyang <1749748955@qq.com>
2025-11-26 17:44:19 +0800

    update

--- script/961d2e44-b063-4ccc-aa6c-803cb5432205/main.R
+++ script/961d2e44-b063-4ccc-aa6c-803cb5432205/main.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(jsonlite)
+library(Hmisc)
 
 params <- fromJSON("params.json")
 
@@ -8,12 +9,8 @@ x_file_metadata <- params$x_file$samples |>
   select(sample_name,group= selcted_group_name )
 
 x_file <- read_tsv(params$x_file$content)|>
-  mutate(MS2_name = case_when(is.na(MS2_name)~MS1_name,
-                              .default =MS2_name )) |>
-  select(MS2_name, x_file_metadata$sample_name) |>
-  na.omit() 
-x_file <- x_file[!duplicated(x_file$MS2_name),] |>
-  column_to_rownames("MS2_name") |>
+  select(feature, x_file_metadata$sample_name) |>
+  column_to_rownames("feature") |>
   t() |> as.data.frame()
 
 
@@ -22,18 +19,21 @@ y_file_metadata <- params$y_file$samples |>
   select(sample_name,group= selcted_group_name )
 
 y_file <- read_tsv(params$y_file$content)|>
-  mutate(MS2_name = case_when(is.na(MS2_name)~MS1_name,
-                              .default =MS2_name )) |>
-  select(MS2_name, y_file_metadata$sample_name) |>
-  na.omit() 
-y_file <- y_file[!duplicated(y_file$MS2_name),] |>
-  column_to_rownames("MS2_name") |>
+  select(feature, y_file_metadata$sample_name) |>
+  column_to_rownames("feature") |>
   t() |> as.data.frame()
 
 feature_list <- str_split(params$query_feature,",")[[1]]
-# feature <- feature_list[1]
 feature_list_all <- intersect(colnames(x_file), colnames(y_file))
 intersect_feature_list <- intersect(feature_list, feature_list_all)
+
+sample_list <- intersect(rownames(x_file), rownames(y_file))
+x_file <- x_file[sample_list,]
+y_file <- y_file[sample_list,]
+
+x_name <- params$x_name
+y_name <- params$y_name
+# feature <- feature_list[1]
 
 point_plot <- function(feature){
   # 手动计算 R²
@@ -48,9 +48,16 @@ point_plot <- function(feature){
   )
   # cor(df$x, df_log$y, method = "pearson")
   
-  fit <- lm(y ~ x, data = df)
-  r2_value <- summary(fit)$r.squared
-  r2_label <- sprintf("R² = %.3f", r2_value)  # 保留 3 位小数，可改
+  # fit <- lm(y ~ x, data = df)
+  # r2_value <- summary(fit)$r.squared
+  # r2_label <- sprintf("R² = %.3f", r2_value)  # 保留 3 位小数，可改
+  # 计算 Spearman 相关系数和 p 值
+  cor_res <- cor.test(df$x, df$y, method = "spearman")
+  rho <- cor_res$estimate
+  pval <- cor_res$p.value
+  label <- sprintf("R= %.3f P= %.3g", rho, pval)
+  
+  
   
   df %>% 
     ggplot(aes(x = x, y = y)) +
@@ -65,7 +72,7 @@ point_plot <- function(feature){
     annotate(
       "text",
       x = Inf, y = Inf,
-      label = r2_label,
+      label = label,
       hjust = 1.1, vjust = 1.5,
       size = 5,
       fontface = "bold"
@@ -82,8 +89,8 @@ point_plot <- function(feature){
       legend.position = "none"
     ) +
     labs(
-      x = "X",
-      y = "Y",
+      x = x_name,
+      y = y_name,
       title = feature
     )
   ggsave(filename = str_glue("output/{feature}.pdf"))
